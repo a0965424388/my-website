9fd268d8127115b2f27dbbec50ac5cf0f45e144a
a0965424388 <a0965424388@gmail.com>
2026-07-08 18:54:08 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+const reveal=document.querySelectorAll(".reveal");
 
+function showReveal(){
+
+reveal.forEach(item=>{
+
+let top=item.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+item.classList.add("active");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll",showReveal);
+
+// 網頁載入時先執行一次
+showReveal();
